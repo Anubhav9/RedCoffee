54a4c2da83fe543aa499f891bf2b66c4abd9c5f5
Anubhav Sanyal <princebest3@rediffmail.com>
2025-02-16 13:25:13 +0530

    Created Support file which will ask users to star the repo

---
+++ support.py
@@ -0,0 +1,9 @@
+import random
+def pick_random_support_message():
+
+    support_messages=[
+        "(✿◕‿◕) 💖 Hey friend! If you like this tool, why not give it a ⭐ on GitHub? It makes me super happy! (≧◡≦). Link: https://github.com/Anubhav9/redcoffee",
+        "(๑˃ᴗ˂)ﻭ 🎉 You’re awesome! If this CLI made your life easier, drop a ⭐ on GitHub to show some love! 🌸. Link: https://github.com/Anubhav9/redcoffee",
+        "(╹◡╹) 💕 This project runs on coffee & GitHub stars! ☕✨ Send some love by starring the repo! ⭐. Link: https://github.com/Anubhav9/redcoffee"
+    ]
+    return random.sample(support_messages,1)
