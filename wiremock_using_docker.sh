767124cc73313bd83b3ef2d324e9bc0b26168100
Anubhav Sanyal <princebest3@rediffmail.com>
2025-03-02 14:47:56 +0530

    added installing requirements in the shell script

--- wiremock_using_docker.sh
+++ wiremock_using_docker.sh
@@ -2,3 +2,4 @@
 docker run -d -p 8000:8080 -v $(pwd)/tests/wiremock/__files:/home/wiremock/__files -v $(pwd)/tests/wiremock/mappings:/home/wiremock/mappings --name=redcoffee-test-wiremock-container wiremock/wiremock:latest
 sleep 10
 docker ps
+pip install -r requirements.txt
