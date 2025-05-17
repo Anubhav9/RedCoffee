22982a385a25338e258f8210bbb729daea336375
Anubhav Sanyal <princebest3@rediffmail.com>
2025-05-17 13:11:14 +0530

    Unit Test File changed to suit the new structure

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -4,20 +4,21 @@ from time import sleep
 from pathlib import Path
 
 import pytest
-from unittest.mock import Mock
-import json
 import subprocess
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from redcoffee import get_issues_by_type, get_duplication_density, get_duplication_map, check_and_validate_file_path
+from core import analyser
+from core.utils.sonarqube_utils import get_duplication_map
+from utils.general_utils import check_and_validate_file_path
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
 
 
 @pytest.fixture(scope="session", autouse=True)
 def spawn_up_wiremock_server():
-    print(f"Trigger Shell Command to bring up WireMock Container")
+    print("Trigger Shell Command to bring up WireMock Container")
     subprocess.call(['sh', './wiremock_using_docker.sh'])
     sleep(10)
     # Example: Start WireMock, setup database connection
+
 
 """"
 def test_get_issues_by_type():
@@ -34,31 +35,36 @@ def test_get_issues_by_type():
     assert impact==expected_impact_list
 
 """
+
+
 def test_duplicate_line_density():
-    duplicate_line_density_actual=get_duplication_density("localhost:8000","car-loan-portal","squ_aa9488007125b09c46e8e2a16a5bccd822738bc2","http")
-    duplicate_line_density_expected="15.0"
-    assert duplicate_line_density_actual==duplicate_line_density_expected,f"Actual Duplicate Line Density and Expected Duplicate Line Density does not match"
+    duplicate_line_density_actual = analyser.get_duplication_density(
+        "localhost:8000", "car-loan-portal", "squ_aa9488007125b09c46e8e2a16a5bccd822738bc2", "http")
+    duplicate_line_density_expected = "15.0"
+    assert duplicate_line_density_actual == duplicate_line_density_expected, "Actual Duplicate Line Density and Expected Duplicate Line Density does not match"
+
 
 def test_duplicate_lines():
-    actual_duplicate_lines_map=get_duplication_map("localhost:8000","car-loan-portal","squ_aa9488007125b09c46e8e2a16a5bccd822738bc2","http")
-    expected_duplicate_lines_map={"app.py":"60"}
-    assert actual_duplicate_lines_map==expected_duplicate_lines_map,f"Difference in Hash Map found for Actual Duplicated Line Map and Expected Duplicated Line Map"
+    actual_duplicate_lines_map = get_duplication_map(
+        "localhost:8000", "car-loan-portal", "squ_aa9488007125b09c46e8e2a16a5bccd822738bc2", "http")
+    expected_duplicate_lines_map = {"app.py": "60"}
+    assert actual_duplicate_lines_map == expected_duplicate_lines_map, "Difference in Hash Map found for Actual Duplicated Line Map and Expected Duplicated Line Map"
 
-@pytest.mark.parametrize("path_name,case",[
-    ("car-loan-report.pdf","file_name_with_pdf"),
-    ("car-loan-report","file_name_without_pdf_extenstion"),
-    ("/Uxser","invalid_directory_name"),
-    ("/Uxser/car-loan-report.pdf","invalid_directory_name_ending_with_pdf"),
-    (Path.home() / "Desktop","random")
+
+@pytest.mark.parametrize("path_name,case", [
+    ("car-loan-report.pdf", "file_name_with_pdf"),
+    ("car-loan-report", "file_name_without_pdf_extenstion"),
+    ("/Uxser", "invalid_directory_name"),
+    ("/Uxser/car-loan-report.pdf", "invalid_directory_name_ending_with_pdf"),
+    (Path.home() / "Desktop", "random")
 ])
-
-def test_path_validation(path_name,case):
-    returned_result=check_and_validate_file_path(path_name)
-    if case=="file_name_wit_pdf":
-        assert returned_result=="car-loan-report.pdf","Mismatch in Path found"
-    elif case=="invalid_directory_name" or case=="invalid_directory_name_ending_with_pdf":
-        resolved_path=Path.home() / "Downloads" / "generated-sonarqube-report.pdf"
-        assert returned_result==resolved_path , "Mismatch in Path found"
-    elif case=="random":
-        resolved_path=Path.home() / "Desktop" / "generated-sonarqube-report.pdf"
-        assert returned_result==resolved_path , "Mismatch in Path found"
+def test_path_validation(path_name, case):
+    returned_result = check_and_validate_file_path(path_name)
+    if case == "file_name_wit_pdf":
+        assert returned_result == "car-loan-report.pdf", "Mismatch in Path found"
+    elif case == "invalid_directory_name" or case == "invalid_directory_name_ending_with_pdf":
+        resolved_path = Path.home() / "Downloads" / "generated-sonarqube-report.pdf"
+        assert returned_result == resolved_path, "Mismatch in Path found"
+    elif case == "random":
+        resolved_path = Path.home() / "Desktop" / "generated-sonarqube-report.pdf"
+        assert returned_result == resolved_path, "Mismatch in Path found"
