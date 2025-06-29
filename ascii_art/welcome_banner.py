e5a20eb4817748d397aef8f840e2fe190edaf85f
Anubhav Sanyal <princebest3@rediffmail.com>
2025-06-29 07:58:30 +0530

    Welcome ASCII Art File Committed

---
+++ ascii_art/welcome_banner.py
@@ -0,0 +1,14 @@
+import pyfiglet
+import colorama
+from ascii_art import constants
+
+def generate_welcome_banner():
+    """
+    Generate a welcome banner using pyfiglet and colorama.
+    """
+    banner = pyfiglet.figlet_format(constants.REDCOFFEE_BANNER_TEXT,end="")
+    colored_banner = colorama.Fore.RED + banner
+    print(colored_banner)
+    sub_text = colorama.Fore.YELLOW + constants.REDCOFFEE_SUB_TEXT
+    print(sub_text)
+
