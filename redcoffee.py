8b4d15654f4bde11fc922665bc233775e630a3cc
Anubhav Sanyal <princebest3@rediffmail.com>
2025-06-29 09:55:17 +0530

    RedCoffee File committed

--- redcoffee.py
+++ redcoffee.py
@@ -1,55 +1,62 @@
-import click
-from support import pick_random_support_message, warning_for_path_change
-from dotenv import load_dotenv
+from diagnose import constants
+import requests
 from utils import general_utils
-from core import analyser
-from diagnose import sanity
-from ascii_art import welcome_banner
 
-load_dotenv()
+def check_all_functioning_parameters(protocol,host,token):
+    """
+    Check if all the parameters are functioning correctly.
+    """    
+    total_checks = 3
+    successful_checks = 0
+    unsuccessful_checks = 3
+    confirm_if_user_token = token.startswith("squ_")
+    if confirm_if_user_token:
+        print(constants.MESSAGE_BASE_SONARQUBE_USER_TOKEN)
+        successful_checks=successful_checks+1
+    else:
+        
+        print(constants.MESSAGE_SONARQUBE_NOT_A_USER_TOKEN)
+        print(constants.MESSAGE_SONARQUBE_USER_TOKEN_HINT_MESSAGE)
+        print(constants.MESSAGE_SONARQUBE_USER_TOKEN_GENERATION_STEPS)
 
+    try:
+        healthcheck_response= requests.get(f"{protocol}{host}{constants.HEALTHCHECK_ENDPOINT}",auth=(token,""))
+        if healthcheck_response.status_code == 200:
+            status = healthcheck_response.json()["health"]
+            if status == "GREEN" or status == "YELLOW":
+                successful_checks=successful_checks+1
+                print(constants.MESSAGE_SONARQUBE_SERVER_REACHABLE)
+                authenticate_token = requests.get(f"{protocol}{host}/api/authentication/validate",auth=(token,""))
+                if authenticate_token.status_code == 200:
+                    token_result = authenticate_token.json()["valid"]
+                    if token_result == True:
+                        successful_checks=successful_checks+1
+                        print(constants.MESSAGE_USER_TOKEN_VALIDATED)
+                    else:
+                        print(constants.MESSAGE_USER_TOKEN_SOMETHING_WRONG)
+                else:
+                    authentication_status_code = authenticate_token.status_code
+                    if authentication_status_code == 401:
+                        print(constants.MESSAGE_USER_TOKEN_401)
+                    elif authentication_status_code == 403:
+                        print(constants.MESSAGE_USER_TOKEN_403)
+                    else:
+                        print(constants.MESSAGE_USER_TOKEN_SOMETHING_WRONG)
+        else:
+            print(constants.MESSAGE_SONARQUBE_SERVER_UNREACHABLE)
+            print(constants.MESSAGE_SONARQUBE_SERVER_UNREACHABLE_HINT_MESSAGE)
+        
+        print(f"Final Diagnosis: {successful_checks} out of {total_checks} checks passed.")
+    except Exception as e:
+        print(constants.MESSAGE_SONARQUBE_SERVER_UNREACHABLE)
+        print(constants.MESSAGE_SONARQUBE_SERVER_UNREACHABLE_HINT_MESSAGE)
+        print(f"Diagnosis Failed. Only able to validate SonarQube User Token. Passed {successful_checks} and rest cannot be validated")
+        
 
-@click.group()
-def cli():
-    pass
-
-
-@click.command()
-@click.option("--host", help="The host url where SonarQube server is running", required=True)
-@click.option("--project", help="Name of the Project Key that we want to search for in SonarQube report ",
-              required=True)
-@click.option("--path", help="Path where we want to the PDF Report", required=False)
-@click.option("--token", help="SonarQube Global Analysis Token", required=True)
-@click.option("--protocol", type=click.Choice(["http", "https"], case_sensitive=False), required=False,
-              help="The protocol that you want to enforce - HTTP or HTTPS")
-def generatepdf(host, project, path, token, protocol):
-    welcome_banner.generate_welcome_banner()
-    resolved_path = str(general_utils.check_and_validate_file_path(path))
-    analyser.generate_final_report_and_transmit_to_sentry(
-        resolved_path, host, project, token, protocol)
-    print(pick_random_support_message())
-    if path != resolved_path:
-        print(warning_for_path_change(resolved_path))
-
-@click.command()
-@click.option("--host", help="The host url where SonarQube server is running", required=True)
-@click.option("--token", help="SonarQube Global Analysis Token", required=True)
-@click.option("--protocol", type=click.Choice(["http", "https"], case_sensitive=False), required=False,
-              help="The protocol that you want to enforce - HTTP or HTTPS")
-def diagnose(protocol,host,token):
-    welcome_banner.generate_welcome_banner()
-    new_host = general_utils.remove_protocol(host)
-    new_protocol = general_utils.handle_protocol_for_every_communication(protocol,new_host)
-    sanity.check_all_functioning_parameters(new_protocol,new_host,token)
-
-@click.command()
-def support():
-    welcome_banner.generate_welcome_banner()
-    sanity.connect_for_support()
-
-
-cli.add_command(generatepdf)
-cli.add_command(diagnose)
-cli.add_command(support)
-if __name__ == "__main__":
-    cli()
+def connect_for_support():
+    """
+    Connect for support. Opening Github Issues or shooting an email
+    """
+    print(constants.MESSAGE_SUPPORT_APOLOGIES)
+    print(constants.MESSAGE_SUPPORT_DIAGNOSE_COMMAND)
+    print(constants.MESSAGE_SUPPORT_EMAIL)
