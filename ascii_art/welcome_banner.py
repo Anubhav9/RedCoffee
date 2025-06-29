251be57eebb24c719bb8a74feb982166c13eb0ad
Anubhav Sanyal <princebest3@rediffmail.com>
2025-06-29 08:43:28 +0530

    Changes to Welcome Banner File

--- ascii_art/welcome_banner.py
+++ ascii_art/welcome_banner.py
@@ -6,9 +6,12 @@ def generate_welcome_banner():
     """
     Generate a welcome banner using pyfiglet and colorama.
     """
-    banner = pyfiglet.figlet_format(constants.REDCOFFEE_BANNER_TEXT,end="")
+    banner = pyfiglet.figlet_format(constants.REDCOFFEE_BANNER_TEXT)
     colored_banner = colorama.Fore.RED + banner
-    print(colored_banner)
+    print(colored_banner,end="")
     sub_text = colorama.Fore.YELLOW + constants.REDCOFFEE_SUB_TEXT
     print(sub_text)
+    print("\n")
+    print(colorama.Style.RESET_ALL)
+
 
