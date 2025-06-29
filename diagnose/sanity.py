ab3435918b9b414e590cf0f1c68c89bd707f0215
Anubhav Sanyal <princebest3@rediffmail.com>
2025-06-29 09:54:21 +0530

    Sanity File modified

--- diagnose/sanity.py
+++ diagnose/sanity.py
@@ -1,37 +1,40 @@
 from diagnose import constants
 import requests
+from utils import general_utils
 
 def check_all_functioning_parameters(protocol,host,token):
     """
     Check if all the parameters are functioning correctly.
-    """
+    """    
     total_checks = 3
-    unsuccessful_checks = 0
+    successful_checks = 0
+    unsuccessful_checks = 3
     confirm_if_user_token = token.startswith("squ_")
     if confirm_if_user_token:
         print(constants.MESSAGE_BASE_SONARQUBE_USER_TOKEN)
+        successful_checks=successful_checks+1
     else:
-        unsuccessful_checks += 1
+        
         print(constants.MESSAGE_SONARQUBE_NOT_A_USER_TOKEN)
         print(constants.MESSAGE_SONARQUBE_USER_TOKEN_HINT_MESSAGE)
         print(constants.MESSAGE_SONARQUBE_USER_TOKEN_GENERATION_STEPS)
 
     try:
-        healthcheck_response= requests.get(f"{protocol}://{host}{constants.HEALTHCHECK_ENDPOINT}")
+        healthcheck_response= requests.get(f"{protocol}{host}{constants.HEALTHCHECK_ENDPOINT}",auth=(token,""))
         if healthcheck_response.status_code == 200:
             status = healthcheck_response.json()["health"]
             if status == "GREEN" or status == "YELLOW":
+                successful_checks=successful_checks+1
                 print(constants.MESSAGE_SONARQUBE_SERVER_REACHABLE)
-                authenticate_token = requests.get(f"{protocol}://{host}/api/authentication/validate",auth=(token,""))
+                authenticate_token = requests.get(f"{protocol}{host}/api/authentication/validate",auth=(token,""))
                 if authenticate_token.status_code == 200:
                     token_result = authenticate_token.json()["valid"]
-                    if token_result == "true":
+                    if token_result == True:
+                        successful_checks=successful_checks+1
                         print(constants.MESSAGE_USER_TOKEN_VALIDATED)
                     else:
-                        unsuccessful_checks += 1
                         print(constants.MESSAGE_USER_TOKEN_SOMETHING_WRONG)
                 else:
-                    unsuccessful_checks += 1
                     authentication_status_code = authenticate_token.status_code
                     if authentication_status_code == 401:
                         print(constants.MESSAGE_USER_TOKEN_401)
@@ -40,15 +43,14 @@ def check_all_functioning_parameters(protocol,host,token):
                     else:
                         print(constants.MESSAGE_USER_TOKEN_SOMETHING_WRONG)
         else:
-            unsuccessful_checks += 1
             print(constants.MESSAGE_SONARQUBE_SERVER_UNREACHABLE)
             print(constants.MESSAGE_SONARQUBE_SERVER_UNREACHABLE_HINT_MESSAGE)
         
-        print(f"Final Diagnosis: {total_checks - unsuccessful_checks} out of {total_checks} checks passed.")
+        print(f"Final Diagnosis: {successful_checks} out of {total_checks} checks passed.")
     except Exception as e:
         print(constants.MESSAGE_SONARQUBE_SERVER_UNREACHABLE)
         print(constants.MESSAGE_SONARQUBE_SERVER_UNREACHABLE_HINT_MESSAGE)
-        print(f"Diagnosis Failed. Only able to validate SonarQube User Token. Failed {unsuccessful_checks} and rest cannot be validated")
+        print(f"Diagnosis Failed. Only able to validate SonarQube User Token. Passed {successful_checks} and rest cannot be validated")
         
 
 def connect_for_support():
